ad121ac8e607166ffbd939df7f8cf4200780214c
Linhnvhdev <linhnvhdev@gmail.com>
2023-09-26 23:23:06 +0700

    Add project package

---
+++ Domain/Entities/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Car
+    {
+        public string VinID { get; set; }
+
+        public string ModelID { get; set; }
+        public CarSpecification Model { get; set; }
+
+    }
+}
---
+++ Domain/Entities/CarAccidentHistory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class CarAccidentHistory
+    {
+        public int ID { get; set; }
+
+        public string CarId { get; set; }
+
+        public string UserId { get; set; }
+
+        public User User { get; set; }
+
+        public Car Car { get; set; }
+    }
+}
