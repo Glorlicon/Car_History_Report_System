8efc1b045d06da97d9bf0072c700ebcb1e822b3e
Quang <phamquang1375@gmail.com>
2023-09-29 21:27:41 +0700

    global navigator and footer

--- CarHistoryReportSystem/src/App.tsx
+++ CarHistoryReportSystem/src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import GlobalNavigator from './components/navigator/GlobalNavigator';
+import logo from '../public/logo512.png';
 import './styles/App.css';
+import Footer from './components/footer/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const navItems = [
+        { label: 'Car Sale', link: '/' },
+        { label: 'Car Reports', link: '/about' },
+        {
+            label: 'Car Maintenance', dropdownItems: [
+                { label: 'Find A Service Shop', link: '/service1' },
+                { label: 'Track My Car Maintenance', link: '/service2' }
+            ]
+        },
+        { label: 'Car Values', link: '/contact' }
+    ];
+    return (
+        <div className="App">
+            <GlobalNavigator items={navItems} />
+             
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
