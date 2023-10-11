e539d70c7a96005c9508d793fcc6bd57b0bed122
Linhnvhdev <linhnvhdev@gmail.com>
2023-10-11 15:19:36 +0700

    Change Car Spec API

---
+++ ApplicationCore/DTO/CarSpecification/CarSpecificationParameter.cs
@@ -0,0 +1,14 @@
+﻿using Application.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTO.CarSpecification
+{
+    public class CarSpecificationParameter : PagingParameters
+    {
+
+    }
+}
