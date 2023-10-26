f06440fd39a9579ba8045a648ed7ac26f7303750
Linhnvhdev <linhnvhdev@gmail.com>
2023-10-26 09:42:27 +0700

    Add Car Service History API

---
+++ ApplicationCore/Validation/CarServiceHistory/CarServiceHistoryUpdateRequestDTOValidator.cs
@@ -0,0 +1,18 @@
+﻿using Application.DTO.CarServiceHistory;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validation.CarServiceHistory
+{
+    public class CarServiceHistoryUpdateRequestDTOValidator : AbstractValidator<CarServiceHistoryUpdateRequestDTO>
+    {
+        public CarServiceHistoryUpdateRequestDTOValidator()
+        {
+            RuleFor(x => x.Services).IsInEnum();
+        }
+    }
+}
