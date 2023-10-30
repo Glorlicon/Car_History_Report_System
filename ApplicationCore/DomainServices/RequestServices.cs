3bb48f4935a2a4954ae8fd4a30d877b2ecf03252
datng1412 <datng.1412@gmail.com>
2023-10-30 22:13:30 +0700

    request fix

--- ApplicationCore/DomainServices/RequestServices.cs
+++ ApplicationCore/DomainServices/RequestServices.cs
@@ -1,16 +1,10 @@
 ﻿using Application.Common.Models;
-using Application.DTO.CarSpecification;
 using Application.DTO.Request;
 using Application.Interfaces;
 using AutoMapper;
 using Domain.Entities;
 using Domain.Enum;
 using Domain.Exceptions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.DomainServices
 {
