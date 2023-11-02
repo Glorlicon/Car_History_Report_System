4457be74a89de3954dd67b8f12544387f9f6bbc4
Quang <phamquang1375@gmail.com>
2023-11-02 09:37:54 +0700

    fix navigation bug + refresher initialization bug

--- CarHistoryReportSystem/src/utils/TokenRefresher.tsx
+++ CarHistoryReportSystem/src/utils/TokenRefresher.tsx
@@ -12,42 +12,46 @@ const TokenRefresher = () => {
     const navigate = useNavigate()
     const token = useSelector((state: RootState) => state.auth.token) as unknown as string
 
-    const handleRefresh = async () => {
-        //user hasn't logged in
-        if (!token) return
+   
 
-        if (isTokenExpired()) {
-            dispatch(logout())
-            navigate('/login')
-            return
-        }
-        const response: APIResponse = await RefreshToken(token)
-        if (response.error) {
-            //logout user if fail to refresh token to avoid errors
-            console.log("Failed to refresh token")
-            dispatch(logout())
-            navigate('/login')
-        } else {
-            console.log("Refreshed token")
-            dispatch(setToken(response.data.token))
-        }
-    }
-    //initial run
-    handleRefresh()
     const isTokenExpired = () => {
         const decodedToken = JWTDecoder(token);
         const currentTime = Date.now() / 1000;
-        console.log("expired? ", decodedToken.exp < currentTime)
         return decodedToken.exp < currentTime;
     };
-
-
 
     useEffect(() => {
         let timer: string | number | NodeJS.Timer | undefined
         if (token) {
-            const interval = 60000*4 //every 4 minutes
-            timer = setInterval(handleRefresh, interval)
+            const handleRefresh = async () => {
+                //user hasn't logged in
+                if (!token) {
+                    console.log("No token")
+                    return
+                }
+
+                if (isTokenExpired()) {
+                    console.log("Logout")
+                    dispatch(logout())
+                    navigate('/login')
+                    return
+                }
+                const response: APIResponse = await RefreshToken(token)
+                if (response.error) {
+                    //logout user if fail to refresh token to avoid errors
+                    console.log("Failed to refresh token")
+                    dispatch(logout())
+                    navigate('/login')
+                } else {
+                    console.log("Refreshed token")
+                    dispatch(setToken(response.data.token))
+                }
+            }
+
+            const decodedToken = JWTDecoder(token);
+            const currentTime = Date.now();
+            const interval = 60000 //every 5-1=4 minutes
+            timer = setInterval(handleRefresh, decodedToken.exp * 1000 - currentTime - interval)
         }
         return () => {
             if (timer) clearInterval(timer)
