ec0e1f1cf95c2cbd5150840c85d0200fc47183be
Linhnvhdev <linhnvhdev@gmail.com>
2023-11-09 20:53:08 +0700

    Add Car Inspection History API

---
+++ ApplicationCore/DTO/CarInspectionHistory/CarInspectionHistoryDetailResponseDTO.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTO.CarInspectionHistory
+{
+    public class CarInspectionHistoryDetailResponseDTO
+    {
+        public int Id { get; set; }
+        public int CarInspectionHistoryId { get; set; }
+        public string InspectionCategory { get; set; }
+        public bool IsPassed { get; set; }
+        public string? Note { get; set; }
+    }
+}
