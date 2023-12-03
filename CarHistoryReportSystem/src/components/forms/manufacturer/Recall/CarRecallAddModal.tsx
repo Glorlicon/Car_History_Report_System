d86b58a0661cc8f26d3560ce79b327c588bff4ee
Glorlicon <khoabk1234@gmail.com>
2023-12-04 02:45:20 +0700

    Rework Manufacturer Car Recall List

--- CarHistoryReportSystem/src/components/forms/manufacturer/Recall/CarRecallAddModal.tsx
+++ CarHistoryReportSystem/src/components/forms/manufacturer/Recall/CarRecallAddModal.tsx
@@ -1,24 +1,27 @@
 ﻿import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../../store/State';
 import { CarModel, CarRecalls } from '../../../../utils/Interfaces';
 
 interface CarRecallAddModalCapacityProps {
-    action: "Add" | "Edit"
     recall: CarRecalls,
     models?: CarModel[],
     handleInputChange: (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void
 }
 
 const CarRecallAddModal: React.FC<CarRecallAddModalCapacityProps> = ({
-    action,
     recall,
     models,
     handleInputChange
 }) => {
+    const { t, i18n } = useTranslation();
+    const currentLanguage = useSelector((state: RootState) => state.auth.language);
     return (
         <>
             <div className="manu-car-model-form-columns">
                 <div className="manu-car-model-form-column">
-                    <label>Model</label>
+                    <label>{t('Car Model')}</label>
                     <select name="modelId" value={recall.modelId} onChange={handleInputChange}>
                         {models?.map((m: any, index: number) => (
                             <option key={index} value={m.modelID}>{m.modelID}</option>
@@ -28,7 +31,7 @@ const CarRecallAddModal: React.FC<CarRecallAddModalCapacityProps> = ({
             </div>
             <div className="manu-car-model-form-columns">
                 <div className="ad-car-model-form-column">
-                    <label>Description</label>
+                    <label>{t('Description')}</label>
                     <input type="text" name="description" onChange={handleInputChange} min="0" />
                 </div>
                 {/*ẩn vì date có thể lấy ngày hiện tại luôn*/}
--- CarHistoryReportSystem/src/services/api/Recall.ts
+++ CarHistoryReportSystem/src/services/api/Recall.ts
@@ -1,25 +1,6 @@
 import axios, { AxiosError } from "axios";
 import { APIResponse, CarModel, CarRecalls, CarRecallSearchParams, CarRegistrationSearchParams } from "../../utils/Interfaces";
 
-export async function ListManufacturerRecalls(id: number, token: string): Promise<APIResponse> {
-    try {
-        const response = await axios.get(`${process.env.REACT_APP_BASE_API_URL}/api/CarRecall/manufacturer/${id}`,
-            {
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            })
-        return { data: response.data }
-    } catch (error) {
-        const axiosError = error as AxiosError
-        if (axiosError.code === "ERR_NETWORK") {
-            return { error: "Network error. Please check your internet connection!" }
-        } else {
-            return { error: "Something went wrong. Please try again" }
-        }
-    }
-}
-
 export async function ListManufaturerCarModels(id: number, token: string): Promise<APIResponse> {
     try {
         const response = await axios.get(`${process.env.REACT_APP_BASE_API_URL}/api/CarRecall/manufacturer/${id}`,
@@ -64,15 +45,14 @@ export async function AddCarRecall(data: CarRecalls, token: string): Promise<API
     }
 }
 
-export async function EditCarRecall(data: CarRecalls, token: string): Promise<APIResponse> {
+export async function EditCarRecall(id: number, data: CarRecalls, token: string, connectAPIError: string, language: string): Promise<APIResponse> {
+    console.log(token)
     try {
-        const response = await axios.put(`${process.env.REACT_APP_BASE_API_URL}/api/CarRecall/${data.id}`,
-            {
-                ...data,
-            },
+        const response = await axios.put(`${process.env.REACT_APP_BASE_API_URL}/api/CarRecall/${id}`, data,
             {
                 headers: {
-                    'Authorization': `Bearer ${token}`
+                    'Authorization': `Bearer ${token}`,
+                    'Accept-Language': `${language}`
                 }
             }
         )
@@ -81,9 +61,9 @@ export async function EditCarRecall(data: CarRecalls, token: string): Promise<AP
         const axiosError = error as AxiosError
         console.log("Edit Error!: ", error)
         if (axiosError.code === "ERR_NETWORK") {
-            return { error: "Network error. Please check your internet connection!" }
+            return { error: connectAPIError }
         } else {
-            return { error: "Something went wrong. Please try again" }
+            return { error: (axiosError.response?.data as any).error[0] }
         }
     }
 }
@@ -110,6 +90,77 @@ export async function ListServiceShopCarRecall(token: string, pageNumber: number
         if (axiosError.code === "ERR_NETWORK") {
             return { error: connectAPIError }
         } else {
+            return { error: (axiosError.response?.data as any).error[0] }
+        }
+    }
+}
+
+export async function ListManufacturerRecallss(id: number, token: string, pageNumber: number, connectAPIError: string, language: string, CarRecallSearchParams: CarRecallSearchParams): Promise<APIResponse> {
+    try {
+        const response = await axios.get(`${process.env.REACT_APP_BASE_API_URL}/api/CarRecall/manufacturer/${id}`,
+            {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Accept-Language': `${language}`,
+                    'Cache-Control': 'no-cache'
+                },
+                params: {
+                    PageNumber: pageNumber,
+                    ModelID: CarRecallSearchParams.modelId,
+                    ExpireDateStart: CarRecallSearchParams.recallDateStart,
+                    ExpireDateEnd: CarRecallSearchParams.recallDateEnd
+                }
+            })
+        return { data: response.data, pages: JSON.parse(response.headers['x-pagination']) }
+    } catch (error) {
+        const axiosError = error as AxiosError
+        if (axiosError.code === "ERR_NETWORK") {
+            return { error: connectAPIError }
+        } else {
+            return { error: (axiosError.response?.data as any).error[0] }
+        }
+    }
+}
+
+export async function AddCarRecalls(data: CarRecalls, token: string, connectAPIError: string, language: string): Promise<APIResponse> {
+    try {
+        const response = await axios.post(`${process.env.REACT_APP_BASE_API_URL}/api/CarRecall`, data,
+            {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Accept-Language': `${language}`
+                }
+            }
+        )
+        return { data: response.data }
+    } catch (error) {
+        const axiosError = error as AxiosError
+        console.log("Add Error!: ", error)
+        if (axiosError.code === "ERR_NETWORK") {
+            return { error: connectAPIError }
+        } else {
+            return { error: (axiosError.response?.data as any).error[0] }
+        }
+    }
+}
+
+export async function ImportRegistrationFromExcel(token: string, data: FormData, connectAPIError: string, language: string): Promise<APIResponse> {
+    try {
+        const response = await axios.post(`${process.env.REACT_APP_BASE_API_URL}/api/CarRegistrationHistory/collection/from-csv`, data,
+            {
+                headers: {
+                    'Content-Type': 'multipart/form-data; boundary=--14737809831466499882746641449',
+                    'Authorization': `Bearer ${token}`,
+                    'Accept-Language': `${language}`
+                }
+            })
+        return { data: response.data }
+    } catch (error) {
+        const axiosError = error as AxiosError
+        if (axiosError.code === "ERR_NETWORK") {
+            return { error: connectAPIError }
+        } else {
+            console.log(axiosError)
             return { error: (axiosError.response?.data as any).error[0] }
         }
     }
