1ceea6cd84a3d0f1d7b28d591d8ad7469deb9b6f
K1n9Sta4 <125969259+K1n9Sta4@users.noreply.github.com>
2023-12-04 22:29:18 +0700

    Update CarInspection.ts

--- CarHistoryReportSystem/src/services/api/CarInspection.ts
+++ CarHistoryReportSystem/src/services/api/CarInspection.ts
@@ -18,6 +18,8 @@ export async function ListCarInspection(dataprovider: number, token: string, pag
                     InspectionEndDate: carInspectionSearchParams.endDate
                 }
             })
+        console.log(response)
+        console.log(response.headers)
         return { data: response.data, pages: JSON.parse(response.headers['x-pagination'])}
     } catch (error) {
         const axiosError = error as AxiosError
