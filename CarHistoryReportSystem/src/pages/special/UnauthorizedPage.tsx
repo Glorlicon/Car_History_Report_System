58ae88f34956de75a21894ec6f75207bd278157e
Glorlicon <khoabk1234@gmail.com>
2023-12-06 16:16:41 +0700

    Translation unauthorized page

--- CarHistoryReportSystem/src/pages/special/UnauthorizedPage.tsx
+++ CarHistoryReportSystem/src/pages/special/UnauthorizedPage.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store/State';
 import '../../styles/Unauthorized.css'
 
 function UnauthorizedPage() {
+    const { t, i18n } = useTranslation();
+    const currentLanguage = useSelector((state: RootState) => state.auth.language);
+    useEffect(() => {
+        i18n.changeLanguage(currentLanguage)
+    }, [])
   return (
       <div className="unauthorized-page">
-          <h1>Unauthorized</h1>
-          <p>You are not authorized to see content of this page.</p>
-          <p>If you believe this is a mistake or if you want to appeal, please contact our support team.</p>
+          <h1>{t('Unauthorized')}</h1>
+          <p>{t('UnauthorizedDesc1')}</p>
+          <p>{t('UnauthorizedDesc2')}</p>
       </div>
   );
 }
