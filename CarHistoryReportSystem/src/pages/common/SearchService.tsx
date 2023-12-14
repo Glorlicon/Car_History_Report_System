ca9ef887e190a292a0084c2e01004eb36c417219
K1n9Sta4 <125969259+K1n9Sta4@users.noreply.github.com>
2023-12-14 16:18:46 +0700

    Update SearchService.tsx

--- CarHistoryReportSystem/src/pages/common/SearchService.tsx
+++ CarHistoryReportSystem/src/pages/common/SearchService.tsx
@@ -155,7 +155,8 @@ function SearchService() {
         const response = await fetch(url);
         const data = await response.json();
         if (data.status === "OK") {
-            const { lat, lng } = data.results[0].geometry.location;
+            const lat = parseFloat(data.results[0].geometry.location.lat);
+            const lng = parseFloat(data.results[0].geometry.location.lng);
             return { lat, lng };
         } else {
             throw new Error(data.error_message || 'Failed to geocode address');
